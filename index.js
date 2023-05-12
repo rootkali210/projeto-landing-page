25806d29f9d5bceb6337676208eed595c6ec1e06
Allan Fagundes Oestreich <53247909+rootkali210@users.noreply.github.com>
2023-05-11 22:23:45 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita =window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha =window.document.getElementById("Samantha")
+var Bruna =window.document.getElementById("Bruna")
+var setaEsquerda =window.document.getElementById("seta-esquerda")
+
+
+function RolarParaDireita() {
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+    setaDireita.style ="display:none"
+    setaEsquerda.style ="display:Flex"
+}
+ 
+function RolarParaEsquerda() {
+    Leonardo.style ="display:flex"
+    Bruna.style ="display:none"
+    setaDireita.style ="display.none"
+    setaEsquerda.style ="displayFlex"
+}
